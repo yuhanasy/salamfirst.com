33686c3303499349352f33563da2871288a96d18
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2023-01-05 20:13:13 +0700

    setup tailwindcss

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import React from "react";
 export default async function Page() {
   return (
     <main>
-      <h1>Salamfirst</h1>
+      <h1 className="text-xl font-bold">Salamfirst</h1>
       <Link href="/blog">Visit Blog</Link>
     </main>
   );
