570812f23311a5d8c5339f1e895465c1e2b10a7b
yuhanas yulianto <yulianto.yuhanas@gmail.com>
2023-01-06 14:20:12 +0700

    adjust layout

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,10 @@ import "../styles/globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="bg-white">
+      <body className="flex flex-col items-center mx-4 sm:mx-8 lg:mx-10">
+        <div className="w-full max-w-3xl">{children}</div>
+      </body>
     </html>
   );
 }
